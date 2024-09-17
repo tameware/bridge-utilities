7dd66146b05485a456b35049748ce63b8d0ef236
Adam Wildavsky <adam@tameware.com>
2024-09-16 22:44:18 -0400

    Added unit tests using Mocha and Chai. H/T ChatGPT.

---
+++ test.js
@@ -0,0 +1,80 @@
+// const { expect } = require('chai');
+// const { canonicalVul, parseMatch, parse } = require('./acbl_live_to_bridgewinners.js');
+
+import { expect } from 'chai';
+import { canonicalVul, parseMatch, parse } from './acbl_live_to_bridgewinners.js';
+import { SINGLE_LINE_TEST_INPUT, MULTI_LINE_TEST_INPUT } from './test_strings.js';
+
+describe('canonicalVul', () => {
+  it('should return the correct vulnerability for each board number', () => {
+    expect(canonicalVul(1)).to.equal('-');
+    expect(canonicalVul(2)).to.equal('n');
+    expect(canonicalVul(3)).to.equal('e');
+    expect(canonicalVul(4)).to.equal('b');
+    expect(canonicalVul(5)).to.equal('n');
+    expect(canonicalVul(6)).to.equal('e');
+    expect(canonicalVul(7)).to.equal('b');
+    expect(canonicalVul(8)).to.equal('-');
+    expect(canonicalVul(9)).to.equal('e');
+  });
+});
+
+describe('parseMatch', () => {
+  it('should parse the match correctly and return formatted LIN string', () => {
+    const match = {
+      groups: {
+        cards: 'n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063',
+        dealer: 'n',
+        vul: '-',
+        board: '1',
+        auction: 'pp6hppp',
+        names: 'nn=Ely&sn=Josephine&en=Charles&wn=Helen',
+      },
+    };
+
+    const result = parseMatch(match);
+    const GHAND_STRING = `
+        {ghand n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=n&v=-&b=1&a=pp6hppp&nn=Ely&sn=Josephine&en=Charles&wn=Helen}
+        `.trim()
+
+    expect(result).to.equal(GHAND_STRING);
+  });
+
+  it('should correct the vulnerability if it does not match', () => {
+    const match = {
+      groups: {
+        cards: 'n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063',
+        dealer: 'e',
+        vul: '-',
+        board: '2',
+        auction: 'pp6hppp',
+        names: 'nn=Ely&sn=Josephine&en=Charles&wn=Helen',
+      },
+    };
+
+    const result = parseMatch(match);
+    const GHAND_STRING = `
+{ghand n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=e&v=n&b=2&a=pp6hppp&nn=Ely&sn=Josephine&en=Charles&wn=Helen}
+`.trim()
+
+    expect(result).to.equal(GHAND_STRING);
+  });
+});
+
+describe('parse', () => {
+  it('should parse the source and return formatted ghand strings', () => {
+    const result = parse(SINGLE_LINE_TEST_INPUT);
+    expect(result).to.equal('{ghand n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=n&v=-&b=1&a=pp6hppp&nn=Brian Platnick&sn=Danny Sprung&en=Adam Wildavsky&wn=Ron Gerard}');
+  });
+
+  it('should handle multiple matches and return formatted ghand strings', () => {
+
+    const result = parse(MULTI_LINE_TEST_INPUT);
+    const GHAND_STRING = `
+{ghand n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=n&v=-&b=1&a=pp6hppp&nn=Brian Platnick&sn=Danny Sprung&en=Adam Wildavsky&wn=Ron Gerard}
+{ghand n=caq75daj7skq95h83&s=ck104d964s732haj54&e=cj86dq532sj1086hk10&w=c932dk108sa4hq9762&d=e&v=n&b=2&a=ppp1nppp&nn=Brian Platnick&sn=Danny Sprung&en=Adam Wildavsky&wn=Ron Gerard}
+`.trim()
+
+    expect(result).to.equal(GHAND_STRING);
+  });
+});
---
+++ test_strings.js
@@ -0,0 +1,16 @@
+export const SINGLE_LINE_TEST_INPUT = `handviewer.html?n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=n&v=-&b=1&a=pp6hppp&nn= Brian Platnick &sn= Danny Sprung&en=Adam Wildavsky&wn=Ron Gerard&tbt=y`;
+
+export const MULTI_LINE_TEST_INPUT = `
+    handviewer.html?n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=n&v=-&b=1&a=pp6hppp&nn= Brian Platnick &sn= Danny Sprung&en=Adam Wildavsky&wn=Ron Gerard&tbt=y&p={&lt;p&gt;&lt;a href=&quot;https://live.acbl.org&quot;&gt;ACBL Live&lt;/a&gt;&lt;/p&gt;
+              &lt;p&gt;&lt;b&gt;Event:&lt;/b&gt; 2024%20Summer%20NABC%2C%20VON%20ZEDTWITZ%20LIFE%20MASTER%20PAIRS%2C%20Jul%2021%2C%202024&lt;/p&gt;
+              &lt;p&gt;&lt;b&gt;Final Contract:&lt;/b&gt; 6%21H = Score: 50 &lt;b&gt;Optimal Contract:&lt;/b&gt; %20%2B450%205%21H-NS&lt;/p&gt;
+              &lt;p&gt;&lt;b&gt;Double Dummy N/S:&lt;/b&gt; %205%21C%205%21H%204%21S%204NT%20%21D4 &lt;b&gt;Double Dummy E/W:&lt;/b&gt; %203%21D%20%21C1%20%21H1%20%21S3%20NT2&lt;/p&gt;
+            }">Play&nbsp;</a></td>							<td><a class='' href='/event/NABC242/VZLM/6/board-detail/W?board_num=1'>01</a>							</td>
+                        <td> 6<span class='hearts symbol contract'></span></td>
+                        <td>S</td>
+                        <td>50</td>
+                        <td></td>
+                        <td>30.5</td><td>80</td>							<td class="vs">7 - Brian Platnick - Danny Sprung</td>						</tr>
+                                        <tr>
+                        <td><a class="btn btn-sm btn-play" href="http://www.bridgebase.com/tools/handviewer.html?n=caq75daj7skq95h83&s=ck104d964s732haj54&e=cj86dq532sj1086hk10&w=c932dk108sa4hq9762&d=e&v=-&b=2&a=ppp1nppp&nn= Brian Platnick &sn= Danny Sprung&en=Adam Wildavsky&wn=Ron Gerard&tbt=y&p={&lt;p&gt;&lt;a href=&quot;https://live.acbl.org&quot;&gt;ACBL Live&lt;/a&gt;&lt;/p&gt;
+`;
