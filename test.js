4cd6cb3f4e37843335a5836d61691582332e8415
Adam Wildavsky <adam@tameware.com>
2024-09-18 00:26:29 -0400

    Use sinon to mock the console.

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 // const { canonicalVul, parseMatch, parse } = require('./acbl_live_to_bridgewinners.js');
 
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { canonicalVul, parseMatch, parse } from './acbl_live_to_bridgewinners.js';
 import { SINGLE_LINE_TEST_INPUT, SINGLE_LINE_TEST_OUTPUT, MULTI_LINE_TEST_INPUT, MULTI_LINE_TEST_OUPUT } from './test_strings.js';
 
@@ -20,6 +21,16 @@ describe('canonicalVul', () => {
 });
 
 describe('parseMatch', () => {
+  let mock;
+
+  beforeEach(() => {
+    mock = sinon.mock(console);
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+
   it('should parse the match correctly and return formatted LIN string', () => {
     const match = {
       groups: {
@@ -52,6 +63,11 @@ describe('parseMatch', () => {
       },
     };
 
+    const message = 'Corrected vulnerability on board 2. Was -, should be n';
+    mock.expects('log')
+      .once()
+      .withExactArgs(message);
+
     const result = parseMatch(match);
     const GHAND_STRING = `
 {ghand n=cj532dk10sk103hk542&s=caq1094d-----sq962haqj8&e=c876dq87saj754h97&w=ckdaj965432s8h1063&d=e&v=n&b=2&a=pp6hppp&nn=Ely&sn=Josephine&en=Charles&wn=Helen}
@@ -62,12 +78,27 @@ describe('parseMatch', () => {
 });
 
 describe('parse', () => {
+  let mock;
+
+  beforeEach(() => {
+    mock = sinon.mock(console);
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+
   it('should parse the source and return formatted ghand strings', () => {
     const result = parse(SINGLE_LINE_TEST_INPUT);
     expect(result).to.equal(SINGLE_LINE_TEST_OUTPUT);
   });
 
   it('should handle multiple matches and return formatted ghand strings', () => {
+    const message = 'Corrected vulnerability on board 2. Was -, should be n';
+    mock.expects('log')
+      .once()
+      .withExactArgs(message);
+
     const result = parse(MULTI_LINE_TEST_INPUT);
     expect(result).to.equal(MULTI_LINE_TEST_OUPUT);
   });
