00146246b2fb7c0c56706293c7a651a1c031a5fb
Adam Wildavsky <adam@tameware.com>
2024-09-21 23:00:03 -0400

    Removed MULTI_LINE_TEST_OUPUT_1 and MULTI_LINE_TEST_OUPUT_2. Added a beforeEach() hook.

--- cypress/e2e/convert.cy.js
+++ cypress/e2e/convert.cy.js
@@ -1,19 +1,23 @@
-import { SINGLE_LINE_TEST_INPUT, SINGLE_LINE_TEST_OUTPUT, MULTI_LINE_TEST_INPUT, MULTI_LINE_TEST_OUPUT, MULTI_LINE_TEST_OUPUT_1, MULTI_LINE_TEST_OUPUT_2 } from '../../test_strings.js';
+import { SINGLE_LINE_TEST_INPUT, SINGLE_LINE_TEST_OUTPUT, MULTI_LINE_TEST_INPUT, MULTI_LINE_TEST_OUPUT } from '../../test_strings.js';
+
+function stripNewlines(str) {
+    return str.replace(/\r?\n|\r/g, '');
+}
 
 describe('Utility test', () => {
+  beforeEach(() => {
+    cy.visit('.');
+  });
+
   it('Visits the app and checks the title', () => {
     // cy.visit('http://tameware.com/adam/bridge/utilities/');
     // cy.visit('http://localhost:8080/adam/bridge/utilities/');
     // cy.visit('http://localhost:3000');
-    cy.visit('.');
-
     cy.title().should('include', 'Bridge Utilities');
     cy.get('body').should('contain', 'Prepare ACBL Live results');
   });
 
   it('Confirms that incorrect input produces a null result', () => {
-    cy.visit('.');
-
     cy.get('#userInput').type('hello');
     cy.get('#userInput').should('have.value', 'hello');
     
@@ -23,8 +27,6 @@ describe('Utility test', () => {
   });
 
   it('Confirms that input with one match produces a correct result', () => {
-    cy.visit('.');
-
     cy.get('#userInput').type(SINGLE_LINE_TEST_INPUT);
     cy.get('#userInput').should('include.value', SINGLE_LINE_TEST_INPUT.trim());
     
@@ -36,17 +38,13 @@ describe('Utility test', () => {
   });
 
   it('Confirms that input with two matches produces two correct results', () => {
-    cy.visit('.');
-
     cy.get('#userInput').type(MULTI_LINE_TEST_INPUT);
-    // Doesn't work because the textarea value lacks the newlines in the input
-    // cy.get('#userInput').should('include.value', MULTI_LINE_TEST_INPUT.trim());
+    cy.get('#userInput').should('include.value', stripNewlines(MULTI_LINE_TEST_INPUT.trim()));
     
     cy.get('#textForm').submit()
     
     cy.get('#result').invoke('text').then((editorText) => {
-      expect(editorText).to.contain(MULTI_LINE_TEST_OUPUT_1);
-      expect(editorText).to.contain(MULTI_LINE_TEST_OUPUT_2);
+      expect(editorText).to.contain(stripNewlines(MULTI_LINE_TEST_OUPUT.trim()));
     });
   });
 });
